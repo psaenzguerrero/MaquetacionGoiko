c6ef433596d012c0d590411514d84063c4ca7667
unknown <psaenzguerrero@gmail.com>
2024-10-29 18:29:03 +0100

    colapso

---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let btnA = document.querySelector("label");
+    let desp = document.querySelector(".desplegable");
+    let body = document.querySelector("body");
+    
+    btnA.addEventListener("click",()=>{
+        desp.style.top="0%";
+        desp.style.transition="top 0.5s linear";
+        desp.style.height="100vh";
+        
+        desp.style.backgroundColor="red";
+        body.style.overflow="hidden";
+    });
+
+    let btnC = document.querySelector(".cerrarMenu div:nth-child(1)>svg");
+    btnC.addEventListener("click",()=>{
+        desp.style.top="-100%";
+        desp.style.height="0vh";
+        body.style.overflow="visible";
+    });
+});
